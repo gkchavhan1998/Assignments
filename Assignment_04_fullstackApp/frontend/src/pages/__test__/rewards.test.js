989abe562d23bda7c9a7420571357316afd0c73b
gkchavhan98 <gkchavhan98@gmail.com>
2023-04-05 12:58:39 +0530

    testing code

--- Assignment_04_fullstackApp/frontend/src/pages/__test__/rewards.test.js
+++ Assignment_04_fullstackApp/frontend/src/pages/__test__/rewards.test.js
@@ -1,16 +1,7 @@
 import { render, screen, waitFor } from "@testing-library/react";
-import { wait } from "@testing-library/user-event/dist/utils";
-import { BrowserRouter } from "react-router-dom";
+
 import Rewards from "../rewards";
 import Settings from "../settings";
-
-const MockReward = () => {
-  return (
-    <BrowserRouter>
-      <Rewards />
-    </BrowserRouter>
-  );
-};
 
 describe("Checking input value", () => {
   test("to check rewards component", async () => {
